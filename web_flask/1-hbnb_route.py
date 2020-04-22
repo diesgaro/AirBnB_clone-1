1d4850a8d25577ef2816964cc3e16f16e91bb9c3
Diego Garzon <digarodev@gmail.com>
2020-04-21 22:27:34 -0500

    start a flask web applicacion with two routes

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    Script that starts a flask web application
+"""
+
+from flask import Flask
+
+
+app = Flask("web_flask")
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    hello_hbnb function that returns a string
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    hbnb function that returns a string
+    """
+    return 'HBNB'
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
