043aa7da2bee2cfee9dd8f32675364f16b391bfa
Diego Garzon <digarodev@gmail.com>
2020-04-22 11:01:19 -0500

    implements variables type int in routes

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+    Script that starts a flask web application
+"""
+
+from flask import Flask
+
+
+app = Flask("web_flask")
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    hello_hbnb function that returns a string
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    hbnb function that returns a string
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """
+    c_is_fun function that returns a string
+    """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text):
+    """
+    python_is_cool function that returns a string
+    """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_a_number(n):
+    """
+    is_a_number function that returns a int
+    """
+    return '{} is a number'.format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
